35acb4c2d441795d64ccb0e024934b6346a91f8c
darsam44 <darsam44@gmail.com>
2020-08-26 19:01:05 +0300

    first upload

---
+++ earings.js
@@ -0,0 +1,31 @@
+
+/*
+
+var next = document.querySelector(".next");
+var photo_1 = document.getElementById('er_1');
+
+next.addEventListener("click" , function(){
+  photo_1.src = "images_neckless/earings2_" +3+ ".jpeg";
+  document.querySelector("h1").style.color = "red";
+});
+
+
+var i =1;
+
+var photo_1 = document.querySelectorAll(".show-photos")[0];
+var next = document.querySelector(".next");
+
+next.addEventListener("click", change_photo_1);
+
+
+function change_photo_1(){
+    if (i < 4){
+      i= i+1;
+    }
+    else{
+      i=2;
+    }
+    console.log(i);
+    photo_1.src = "images_neckless/earings2_" +(i-1)+ ".jpeg";
+}
+*/
